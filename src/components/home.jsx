25ab26136e0f4dcb2d6b9ff313a9e52fe8e2d5bd
san00 <c5d59d07@opayq.com>
2018-04-10 18:03:46 +0100

    Create page layout [WIP]

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 import {Link} from "react-router-dom";
-import{ Jumbotron, Grid, Row, Image, Button } from "react-bootstrap";
+import{ Jumbotron, Grid, Row, Image, Button, Col } from "react-bootstrap";
 import "./home.css";
 
 export default class home extends Component {
@@ -10,9 +10,30 @@ export default class home extends Component {
         <Jumbotron>
           <h2>Welcome to ideas</h2>
           <p> Built with React, React-router & React Bootstrap</p>
+          <Link to ="/about">
+        </Link>
         </Jumbotron>
-        <Link to ="/about">
-      </Link>
+        <Row className = " show-grid text-center">
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/rawpixel-com-585640-unsplash.jpg" rounded className="site-pic"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/jon-tyson-228428-unsplash.jpg" rounded className="site-pic"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/brooke-lark-609902-unsplash.jpg" rounded className="site-pic"/>
+        </Col>
+        </Row>
+        <Row className = "show-grid text-center about-section">
+          <h3 className = "about-section-header" >A little about me</h3>
+          <Col xs={12} sm={6} md={4} >
+            <Image src="assets/rawpixel-com-585640-unsplash.jpg" rounded className="site-pic"/>
+          </Col>
+          <Col>
+            <p>front end developer</p>
+            <h4>Sky runner</h4>
+            <p >Everyone needs a friend. Friends are the most valuable things in the world. If I paint something, I don't want to have to explain what it is</p></Col>
+        </Row>
       </Grid>
     )
   }
