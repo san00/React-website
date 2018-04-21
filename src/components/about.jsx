245adc50ee5be887e4d1cab730707ef47cee8ddf
san00 <c5d59d07@opayq.com>
2018-04-21 20:02:36 +0100

    Add services section to about page layout[WIP]

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import {Grid, Image, Col, PageHeader, Row} from "react-bootstrap";
+import {Grid, Image, Col, PageHeader, Row, Button} from "react-bootstrap";
 import "./about.css";
 
 export default class about extends Component {
@@ -20,6 +20,31 @@ export default class about extends Component {
           <span class="icons fab fa-github"></span>
           <span class="icons fab fa-dribbble"> </span>
         </Col>
+        <Col xs={12} sm={12} smOffset={2} className="services-header"><h4>Services</h4></Col>
+          <Col xs={12} sm={4} smOffset={2} className="services">
+            <span class="icons fas fa-camera-retro "></span>
+            <h5>Photography</h5>
+            <p>Happy painting, God bless. In this world, everything can be happy. Don't fiddle with it all day. Any little thing can be your friend if you let it be</p>
+            <Button>Learn more</Button>
+          </Col>
+          <Col xs={12} sm={4}  smOffset={1} className="services">
+            <span class="icons fas fa-laptop "></span>
+            <h5>Web Development</h5>
+            <p>Happy painting, God bless. In this world, everything can be happy. Don't fiddle with it all day. Any little thing can be your friend if you let it be</p>
+            <Button>Learn more</Button>
+          </Col>
+          <Col xs={12} sm={4} smOffset={2} className="services">
+            <span class="icons fas fa-pencil-alt "></span>
+            <h5>Design</h5>
+            <p>Happy painting, God bless. In this world, everything can be happy. Don't fiddle with it all day. Any little thing can be your friend if you let it be</p>
+            <Button>Learn more</Button>
+          </Col>
+          <Col xs={12} sm={4}  smOffset={1}className="services">
+            <span class="icons far fa-folder-open"></span>
+            <h5>Consultancy</h5>
+            <p>Happy painting, God bless. In this world, everything can be happy. Don't fiddle with it all day. Any little thing can be your friend if you let it be</p>
+            <Button>Learn more</Button>
+          </Col>
       </Grid>
     </div>)
   }
