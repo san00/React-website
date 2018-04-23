98d369aaa2f22f4982bfe2dee35224e53de6eb1d
san00 <c5d59d07@opayq.com>
2018-04-23 16:11:04 +0100

    Create work/portfolio component

---
+++ src/components/work.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+import {Link} from "react-router-dom";
+import{Grid, Row, Image, Col } from "react-bootstrap";
+import "./work.css";
+
+export default class work extends Component {
+  render() {
+    return(
+      <Grid>
+        <Row className = " show-grid text-center">
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/rawpixel-com-585640-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/autumn-studio-394136-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/simon-matzinger-320332-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={4} className="image-wrapper">
+            <Image src="assets/autumn-studio-394136-unsplash Cropped.jpg" className="homePage-pics"/>
+          </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/simon-matzinger-320332-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/rawpixel-com-585640-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        </Row>
+        <Row>
+
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/simon-matzinger-320332-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/rawpixel-com-585640-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        <Col xs={12} sm={4} className="image-wrapper">
+          <Image src="assets/autumn-studio-394136-unsplash Cropped.jpg" className="homePage-pics"/>
+        </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
