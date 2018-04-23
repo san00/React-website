07049b361576f00deb08464e6891a7fcf34f8c5b
san00 <c5d59d07@opayq.com>
2018-04-23 19:24:17 +0100

    import grid to home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 import {Link} from "react-router-dom";
-import{ Jumbotron } from "react-bootstrap";
+import{ Jumbotron, Grid } from "react-bootstrap";
 import "./home.css";
 
 export default class home extends Component {
